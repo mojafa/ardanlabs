fe91603d273c55f2c4fb2aa07c9faf88514dc33f
MoJafa <jmohsa@gmail.com>
2023-04-21 13:02:30 +0300

    added go palindrome code

--- practical-go/palindrome/palindrome.go
+++ practical-go/palindrome/palindrome.go
@@ -1,12 +1,24 @@
 package main
 
-// isPalindrome ("g") -> true
-// isPalindrome ("go") -> false
-// isPalindrome ("gog") -> true
-// isPalindrome ("gogo") -> false
+import "fmt"
 
-// func isPalindrome(s string) bool {
-// TODO ASCII characters
-// TODO Unicode characters
-// }
-func main() {}
+func isPalindrome(s string) bool {
+	// make it unicode aware
+	rs := []rune(s)
+
+	for i := 0; i < len(rs)/2; i++ {
+		if rs[i] != rs[len(rs)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome("anna"))
+	fmt.Println(isPalindrome("12321"))
+	fmt.Println(isPalindrome("Arewenotdrawnonwardwefewdrawnonwardtonewera"))
+	fmt.Println(isPalindrome("not a palindrome"))
+	fmt.Println(isPalindrome("'Are we not drawn onward, we few, drawn onward to new era? ' - Socrates Setarcos"))
+
+}
